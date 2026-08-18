a9258f36c9cf9791c83ee758a41730f3a72383cc
Somesh <abc@gmail.com>
2026-08-18 14:16:44 +0530

    Fix CORS frontend origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://task-management-frontend-4in.onrender.com',
+      'https://task-management-frontend-g4in.onrender.com',
     ],
   });
 
